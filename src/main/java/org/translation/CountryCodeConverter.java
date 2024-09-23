c967259a38bda0ff9b17b07122a347e52d479144
JonDD-D <dengjohn123@163.com>
2024-09-23 18:44:47 -0400

    CountryCodeConverter edition

--- src/main/java/org/translation/CountryCodeConverter.java
+++ src/main/java/org/translation/CountryCodeConverter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -11,12 +12,9 @@ import java.util.List;
  */
 public class CountryCodeConverter {
 
-    // TODO Task: pick appropriate instance variable(s) to store the data necessary for this class
+    private HashMap<String, String[]> map_three;
+    private HashMap<String, String[]> map_country_name;
 
-    /**
-     * Default constructor which will load the country codes from "country-codes.txt"
-     * in the resources folder.
-     */
     public CountryCodeConverter() {
         this("country-codes.txt");
     }
@@ -26,13 +24,24 @@ public class CountryCodeConverter {
      * @param filename the name of the file in the resources folder to load the data from
      * @throws RuntimeException if the resource file can't be loaded properly
      */
+    @SuppressWarnings("checkstyle:WhitespaceAfter")
     public CountryCodeConverter(String filename) {
-
+        this.map_three = new HashMap<>();
+        this.map_country_name = new HashMap<>();
         try {
+            // TODO Task: pick appropriate instance variable(s) to store the data necessary for this class
             List<String> lines = Files.readAllLines(Paths.get(getClass()
                     .getClassLoader().getResource(filename).toURI()));
-
             // TODO Task: use lines to populate the instance variable(s)
+            for (String line : lines) {
+                String[] store = line.split("\t");
+                String[] country_name_for_key;
+                String[] countru_code_for_key;
+                country_name_for_key = new String[]{store[1],store[2],store[3]};
+                countru_code_for_key = new String[]{store[0],store[1],store[3]};
+                map_country_name.put(store[0], country_name_for_key);
+                map_three.put(store[2], countru_code_for_key);
+            }
 
         }
         catch (IOException | URISyntaxException ex) {
@@ -48,7 +57,8 @@ public class CountryCodeConverter {
      */
     public String fromCountryCode(String code) {
         // TODO Task: update this code to use an instance variable to return the correct value
-        return code;
+        String key = code.toUpperCase();
+        return map_three.get(key)[0];
     }
 
     /**
@@ -58,7 +68,7 @@ public class CountryCodeConverter {
      */
     public String fromCountry(String country) {
         // TODO Task: update this code to use an instance variable to return the correct value
-        return country;
+        return map_country_name.get(country)[1];
     }
 
     /**
@@ -67,6 +77,6 @@ public class CountryCodeConverter {
      */
     public int getNumCountries() {
         // TODO Task: update this code to use an instance variable to return the correct value
-        return 0;
+        return map_country_name.size() - 1;
     }
 }
